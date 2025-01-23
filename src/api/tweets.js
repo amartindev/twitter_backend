5cbddc9f275158c1c78edacc1bf18d415710a577
Antonio Martin <luis.martinf89@gmail.com>
2025-01-23 16:48:52 -0500

    serverless

--- src/api/tweets.js
+++ src/api/tweets.js
@@ -15,39 +15,40 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/api/tweets", async (req, res) => {
-  return res.json({ message: "API is working" });
+  return res.send({ message: "API is working" });
   })
 // Endpoint para obtener tweets
 app.post("/api/tweets", async (req, res) => {
-    const { username } = req.body;
-    const accessToken = process.env.TWITTER_ACCESS_TOKEN;
+  return res.send({ message: "API is working EN POST" });
+  // const { username } = req.body;
+    // const accessToken = process.env.TWITTER_ACCESS_TOKEN;
 
-    if (!username) {
-        return res.status(400).json({ error: "Username is required" });
-    }
+    // if (!username) {
+    //     return res.status(400).json({ error: "Username is required" });
+    // }
 
-    try {
-        const response = await axios.get(
-            "https://api.twitter.com/2/tweets/search/recent",
-            {
-                params: { query: `from:${username}` },
-                headers: { Authorization: `Bearer ${accessToken}` },
-            }
-        );
-        res.json(response.data);
-    } catch (error) {
-        // Manejar específicamente el error 429
-        if (error.response && error.response.status === 429) {
-            // Si el error es 429, puedes devolver un mensaje más adecuado
-            return res
-                .status(429)
-                .json({ error: "Too many requests. Please try again later." });
-        }
+    // try {
+    //     const response = await axios.get(
+    //         "https://api.twitter.com/2/tweets/search/recent",
+    //         {
+    //             params: { query: `from:${username}` },
+    //             headers: { Authorization: `Bearer ${accessToken}` },
+    //         }
+    //     );
+    //     res.json(response.data);
+    // } catch (error) {
+    //     // Manejar específicamente el error 429
+    //     if (error.response && error.response.status === 429) {
+    //         // Si el error es 429, puedes devolver un mensaje más adecuado
+    //         return res
+    //             .status(429)
+    //             .json({ error: "Too many requests. Please try again later." });
+    //     }
 
-        // En caso de otros errores, se maneja normalmente con el código 500
-        console.error(error);
-        res.status(500).json({ error: error.message });
-    }
+    //     // En caso de otros errores, se maneja normalmente con el código 500
+    //     console.error(error);
+    //     res.status(500).json({ error: error.message });
+    // }
 });
 // Configuración para entorno live (comenta esta sección si usas serverless)
 // const PORT = process.env.PORT || 5000;
